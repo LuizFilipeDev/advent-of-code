14ddba1a7f801f1b79a7e13422c28e2d4b0c6194
LuizFilipeDev <luizfilipe.souza.s@outlook.com>
2023-06-20 20:07:57 -0300

    setup the environment

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AdventOfCode{
+
+    /*
+
+    Advent of Code is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. 
+    People use them as interview prep, company training, university coursework, practice problems, a speed contest, or to challenge each other.    
+    
+    */
+
+    public class Program{
+        public static void Main(string[] args){
+
+            AdventOfCode2015.DayOne.Execute();
+
+        }
+    }
+}
